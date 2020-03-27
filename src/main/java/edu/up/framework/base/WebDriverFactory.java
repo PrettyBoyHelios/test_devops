e84146ca589d4ab8751bd96a1641e49adc6387f1
Oscar Garcia <ogcdevelopment@outlook.com>
2020-03-27 02:19:01 -0500

    Refactoring to a factory for driver

--- src/main/java/edu/up/framework/base/WebDriverFactory.java
+++ src/main/java/edu/up/framework/base/WebDriverFactory.java
@@ -9,36 +9,62 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 public class WebDriverFactory {
     private final static Logger logger = LoggerFactory.getLogger(WebDriverFactory.class);
+    private static String WINDOWS_DRIVER = "/chromedriver/windows/chromedriver.exe";
+    private static String MAC_DRIVER = "/chromedriver/mac/chromedriver";
+
     private static int iPosX;
     private static int iPosY;
 
-    static WebDriver createInstance(){
-        WebDriver driver;
-        DesiredCapabilities desiredCapabilities;
-        System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+    public static WebDriver getWebDriver() {
+        if (System.getProperty("os.name").contains("Mac")) {
+            File cDriver = new File(WebDriverFactory.class.getResource(MAC_DRIVER).getFile());
+
+            // Is it executable
+            if (!cDriver.canExecute()) {
+                cDriver.setExecutable(true);
+            }
+            System.setProperty("webdriver.chrome.driver", WebDriverFactory.class.getResource(MAC_DRIVER).getFile());
+
+            // Now checking for existence of Chrome executable.'
+            if (!new File("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome").exists()) {
+                throw new RuntimeException();
+            }
+        } else{
+            String sDriver =WebDriverFactory.class.getResource(WINDOWS_DRIVER).getFile();
+            System.setProperty("webdriver.chrome.driver", urlDecode(sDriver) );
+        }
+
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--ignore-certificate-errors");
         options.addArguments("test-type");
         options.addArguments("disable-popup-blocking");
         options.addArguments("disable-extensions");
-        desiredCapabilities = DesiredCapabilities.chrome();
-        desiredCapabilities.setJavascriptEnabled(true);
-        desiredCapabilities.setBrowserName("chrome");
-        driver = new ChromeDriver(options);
+        WebDriver d = new ChromeDriver(options);
+        positionBrowserWindow(d);
+        return d;
+    }
 
+    private static void positionBrowserWindow(WebDriver driver){
         Random rn = new Random();
         iPosX = rn.nextInt(600 - 50 + 1) + 50;
         iPosY = rn.nextInt(100 - 10 + 1) + 40;
         driver.manage().window().setPosition(new Point(iPosX, iPosY));
         driver.manage().window().setSize(new Dimension(1000, 800));
-        logger.info("Thread id = " + Thread.currentThread().getId());
-        logger.info("Hashcode failure Web Driver: " + driver.hashCode());
-        logger.info("Browser selected: "+desiredCapabilities.getBrowserName());
+    }
 
-
-        return  driver;
+    private static String urlDecode(String value)  {
+        try {
+            return java.net.URLDecoder.decode(value, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return value;
     }
 }
--- src/test/java/edu/up/tests/SimpleTestSelenium.java
+++ src/test/java/edu/up/tests/SimpleTestSelenium.java
@@ -1,5 +1,6 @@
 package edu.up.tests;
 
+import edu.up.framework.base.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,12 +11,10 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 public class SimpleTestSelenium {
-    private static String WINDOWS_DRIVER = "/chromedriver/windows/chromedriver.exe";
-    private static String MAC_DRIVER = "/chromedriver/mac/chromedriver";
 
     @Test
     public void ShouldOpenBrowser() {
-        WebDriver driver = setupChromeDriver();
+        WebDriver driver = WebDriverFactory.getWebDriver();
         driver.navigate().to("https://www.up.edu.mx/es");
         driver.close();
     }
@@ -25,38 +24,5 @@ public class SimpleTestSelenium {
         /* TODO */
         /*Add code to obtain the number of links and print to console*/
 
-    }
-    private WebDriver setupChromeDriver() {
-        if (System.getProperty("os.name").contains("Mac")) {
-            File cDriver = new File(SimpleTestSelenium.class.getResource(MAC_DRIVER).getFile());
-
-            // Is it executable
-            if (!cDriver.canExecute()) {
-                cDriver.setExecutable(true);
-            }
-            System.setProperty("webdriver.chrome.driver", SimpleTestSelenium.class.getResource(MAC_DRIVER).getFile());
-
-            // Now checking for existence of Chrome executable.'
-            if (!new File("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome").exists()) {
-                throw new RuntimeException();
-            }
-        } else{
-            String sDriver =SimpleTestSelenium.class.getResource(WINDOWS_DRIVER).getFile();
-            System.setProperty("webdriver.chrome.driver", urlDecode(sDriver) );
-        }
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--ignore-certificate-errors");
-        return new ChromeDriver(options);
-    }
-
-    private String urlDecode(String value)  {
-        try {
-            return java.net.URLDecoder.decode(value, StandardCharsets.UTF_8.name());
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return value;
     }
 }
